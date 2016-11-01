1ad84f71dab2060cbf89f839a898ec88d0042b7b
Jameson Nash <vtjnash@gmail.com>
2016-11-01 10:44:50 -0400

    re-design sysimg serialization to work inplace and non-recursive
    we use this step to try to maximize the number of shared mmap pages
    and minimize memory (virtual and physical) that is required.
    
    the sorting order appears to have a strong impact on the time for a full gc

--- src/jitlayers.cpp
+++ src/jitlayers.cpp
@@ -1161,9 +1161,9 @@ static void jl_gen_llvm_globaldata(llvm::Module *mod, ValueToValueMapTy &VMap,
     if (sysimg_data) {
         Constant *data = ConstantDataArray::get(jl_LLVMContext,
             ArrayRef<uint8_t>((const unsigned char*)sysimg_data, sysimg_len));
-        addComdat(new GlobalVariable(*mod, data->getType(), true,
+        addComdat(new GlobalVariable(*mod, data->getType(), false,
                                      GlobalVariable::ExternalLinkage,
-                                     data, "jl_system_image_data"));
+                                     data, "jl_system_image_data"))->setAlignment(64);
         Constant *len = ConstantInt::get(T_size, sysimg_len);
         addComdat(new GlobalVariable(*mod, len->getType(), true,
                                      GlobalVariable::ExternalLinkage,
